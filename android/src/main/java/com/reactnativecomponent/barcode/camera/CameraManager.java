0339c447c462612614aa17b690384923769de8ec
Nghi-NV <32028055+Nghi-NV@users.noreply.github.com>
2019-08-26 10:21:44 +0700

    Update CameraManager.java
    check  camera auto focus exception

--- android/src/main/java/com/reactnativecomponent/barcode/camera/CameraManager.java
+++ android/src/main/java/com/reactnativecomponent/barcode/camera/CameraManager.java
@@ -223,8 +223,11 @@ private final Context context;
     if (camera != null && previewing) {
       autoFocusCallback.setHandler(handler, message);
       //Log.d(TAG, "Requesting auto-focus callback");
-      camera.autoFocus(autoFocusCallback);
-
+      try {
+	        camera.autoFocus(autoFocusCallback);
+	    } catch (Exception e) {
+	
+	    }
     }
   }
 
